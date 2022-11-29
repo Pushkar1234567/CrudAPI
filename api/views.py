22ea8db743640d97cbd6f77db2ac70b3862bc7c8
pushkar_2001 <pushkar.saini@algoworks.com>
2022-11-29 16:05:18 +0530

    commit003

--- api/views.py
+++ api/views.py
@@ -4,22 +4,25 @@ from .models import meriDukan
 from .serializer import dukanSerializer
 
 class searchDukan(views.APIView):
-    def get(self,request,pk=None):
-        if pk is not None:
-            obj=meriDukan.objects.filter(pk=pk).first()
-            serialize=dukanSerializer(obj)
-            return Response(serialize.data)
+    def get(self,request):
         obj=meriDukan.objects.all()
         serialize=dukanSerializer(obj,many=True)
         return Response(serialize.data)
+
+    def retrieve(self,request,pk):
+        obj=meriDukan.object.filter(id=pk).first()
+        if not obj:
+            return Response("Not data present")
+        serializer=dukanSerializer(obj)
+        return Response(serializer.data)
+
     def post(self,request):
-        data=request.data
-        serializer=dukanSerializer(data=data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response({'msg':'Data successfully Created'})
-        return Response (serializer.errors)
-    def put(self,request,pk):
+            serializer=dukanSerializer(data=request.data, is_tombstoned=True)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                return Response({'msg':'Data successfully Created'})
+            return Response (serializer.errors)
+    def update(self,request,pk):
         if pk is not None:
             obj=meriDukan.objects.filter(pk=pk).first()
             serilarzer=dukanSerializer(obj,data=request.data)
@@ -27,8 +30,8 @@ class searchDukan(views.APIView):
             serilarzer.save()
             return Response("data successfully Updated")
         return Response("Your entered id is not here")
-    def delete(self,request,pk):
-        obj=meriDukan.objects.filter(pk=pk).first()
+    def destroy(self,request,pk):
+        obj=meriDukan.objects.filter(id=pk,is_tombstoned=True).first()
         obj.delete()
         return Response("User deleted")
 
